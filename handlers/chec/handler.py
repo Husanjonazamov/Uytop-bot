27702cae962add141118b5c1f0a8c5b994de1c2b
Husanjonazamov <azamovghusanboy08@gmail.com>
2026-02-20 20:09:09 +0500

    serach tigurkladfi

--- handlers/chec/handler.py
+++ handlers/chec/handler.py
@@ -8,8 +8,16 @@ from utils.env import ADMIN
 
 @dp.callback_query_handler(lambda c: c.data.startswith("check_"))
 async def handle_check_callback(callback_query: CallbackQuery):
-    listing_id = int(callback_query.data.split("_")[1])
-    tg_id = int(callback_query.data.split("_")[2])
+    data = callback_query.data.split("_")
+    
+    if data[1] == "edit":
+        await callback_query.answer("Ushbu e'lon allaqachon tasdiqlangan.")
+        return
+
+    listing_id = int(data[1])
+    tg_id = int(data[2])
+    
+    await callback_query.answer()
     
     await bot.edit_message_reply_markup(
         chat_id=ADMIN,
